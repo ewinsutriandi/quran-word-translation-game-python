9ea2dd7df897b75af3944996a44b41c90862d413
ewinsutriandi <ewin.sutriandi@gmail.com>
2023-03-03 15:07:45 +0800

    generate ayah to word idx map for faster processing

--- sandbox.py
+++ sandbox.py
@@ -1,16 +1,28 @@
-import generators as g
-import game
+import loader
 
-surah = game.pick_surah_all()
+ayah_all = loader.load_ayah_all()
+word_all = loader.load_word_all()
 
-'''
-gen = g.GenerateMidDiffQuizFromSingleSura(surah)
+ayah_word_map = {}
 
-ql = gen.generateQuizList()
+for k,v in word_all.items():
+    ayah_idx = str(v["ayah"])
+    ayah_word = {"start":int(k), "end":int(k)}
+    if ayah_idx in ayah_word_map:
+        ayah_word = ayah_word_map[str(ayah_idx)]
+        st_idx = int(ayah_word["start"])
+        ed_idx = int(ayah_word["end"])
+        if int(k) < st_idx:
+            ayah_word["start"] = int(k)
+        if int(k) > ed_idx:
+            ayah_word["end"] = int(k)    
+    ayah_word_map[str(ayah_idx)] = ayah_word
 
-for q in ql:
-    print(q.question)
-    print("jawaban:",q.correct_answer)
-    print()
+for i in range(1,25):
+    ayah_word = ayah_word_map[str(i)] 
+    print(ayah_word)
 
-'''
+import json
+
+with open('qres/ayah-word-map.json', 'w') as fp:
+    json.dump(ayah_word_map, fp, indent=4)
